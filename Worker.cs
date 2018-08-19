2e3ef2565eda93ed3ff64ddb8465c99193d80ad0
Fred Duarte <fduartej@gmail.com>
2018-08-18 19:05:50 -0500

    Employee Constructor And Salary

--- Worker.cs
+++ Worker.cs
@@ -5,6 +5,19 @@ namespace helloworld
 
     class Worker : Employee{
 
+        public Worker(int id, string firstName,
+            string lastName, int age, int tarifaHora) :
+            base(id, firstName, lastName, age, tarifaHora)
+        {
+
+        }
+
+        public override void CalcularSalario()
+        {
+
+            Salary = TarifaHora * 40 * 1;
+        }
+
     }
 
 }
